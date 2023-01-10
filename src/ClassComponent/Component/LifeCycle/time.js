1fa4a4412b62cdfc414298cd11b8876624a19b7c
carmelraj <carmelraj601@gmail.com>
2023-01-10 20:57:53 +0530

    1 file modified

--- src/ClassComponent/Component/LifeCycle/time.js
+++ src/ClassComponent/Component/LifeCycle/time.js
@@ -1,8 +1,15 @@
 import React,{Component} from 'react';
 class TimeLifeCycle extends Component{
+  constructor(){
+    super()
+    this.state = {
+      date:new Date()
+    }
+  }
   render(){
     return(<>
       <h1>Time</h1>
+      {this.state.date.toLocaleTimeString()}
     </>);
   }
 }
