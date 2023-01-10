298e3adc5c8022c33f69b1f12b9a10f242f4eb86
carmelraj <carmelraj601@gmail.com>
2023-01-10 21:06:39 +0530

    1 file modified

--- src/ClassComponent/Component/LifeCycle/counter.js
+++ src/ClassComponent/Component/LifeCycle/counter.js
@@ -1,9 +1,15 @@
 import React,{Component} from 'react';
 class CounterLifeCycle extends Component{
+  constructor(){
+    super()
+    this.state={
+      count: 0
+    }
+  }
   render(){
     return(<>
       <h1>Counter Life Cycle</h1>
-      
+       Counter : {this.state.count} 
     </>);
   }
 }
