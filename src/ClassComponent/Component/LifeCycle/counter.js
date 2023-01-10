ef7a0ed50a0e31805f4d79c7ab77bca523c69531
carmelraj <carmelraj601@gmail.com>
2023-01-10 21:13:55 +0530

    1 file modified

--- src/ClassComponent/Component/LifeCycle/counter.js
+++ src/ClassComponent/Component/LifeCycle/counter.js
@@ -6,10 +6,16 @@ class CounterLifeCycle extends Component{
       count: 0
     }
   }
+
+  
+
+
   render(){
     return(<>
       <h1>Counter Life Cycle</h1>
-       Counter : {this.state.count} 
+       <p>Counter : {this.state.count} </p>
+       <button onClick={this.increment}>Increment</button>&nbsp;
+       <button onClick={this.decrement}>Decrement</button>
     </>);
   }
 }
