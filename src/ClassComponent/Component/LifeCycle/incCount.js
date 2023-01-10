ece597037dbd0670e788bede33c181e8e3188814
carmelraj <carmelraj601@gmail.com>
2023-01-10 22:59:19 +0530

    1 file modified

--- src/ClassComponent/Component/LifeCycle/incCount.js
+++ src/ClassComponent/Component/LifeCycle/incCount.js
@@ -8,20 +8,20 @@ class IncCount extends Component{
     this.reset =()=>this.setState({count:0});
   }
 
-  componentDidMount(){
-    this.incCount = setInterval(
-      ()=>this.inc(),
-      1000
-    );
-  }
+  // componentDidMount(){
+  //   this.incCount = setInterval(
+  //     ()=>this.inc(),
+  //     1000
+  //   );
+  // }
 
-  inc(){
-    this.setState({count:this.state.count+1});
-  }
+  // inc(){
+  //   this.setState({count:this.state.count+1});
+  // }
 
-  componentWillUnmount(){
-    clearInterval(this.incCount);
-  }
+  // componentWillUnmount(){
+  //   clearInterval(this.incCount);
+  // }
   render(){
     return(<>
       <h1>Increment Count</h1>
