762151c1c1eedace718e2fd291b856b70f505490
carmelraj <carmelraj601@gmail.com>
2023-01-10 22:48:53 +0530

    1 file modified

--- src/ClassComponent/Component/LifeCycle/incCount.js
+++ src/ClassComponent/Component/LifeCycle/incCount.js
@@ -2,11 +2,28 @@ import React,{Component} from 'react';
 class IncCount extends Component{
   constructor(props){
     super(props)
+    this.state={
+      count:0
+    }
+  }
+
+  componentDidMount(){
+    this.incCount = setInterval(
+      ()=>this.inc(),
+      1000
+    );
+  }
+
+  inc(){
+    this.setState({count:this.state.count+1});
+  }
+  componentWillUnmount(){
+    clearInterval(this.incCount);
   }
   render(){
     return(<>
       <h1>Increment Count</h1>
-
+        Count : {this.state.count}
     </>);
   }
 }
