31b65bb699cc3e8b8515418f5fac8d3d7059d533
carmelraj <carmelraj601@gmail.com>
2023-01-11 18:11:18 +0530

    2 files modified

--- src/funcComponent/memo/memoComp.js
+++ src/funcComponent/memo/memoComp.js
@@ -1,7 +1,8 @@
 import React from 'react';
-const FuncMemoCompo = ()=>{
+const FuncMemoCompo = ({name})=>{
   return(<>
      <p>Memo Component</p>
+     -{name}-
   </>);
 }
 export default FuncMemoCompo
--- src/funcComponent/memo/parent.js
+++ src/funcComponent/memo/parent.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React,{useState} from 'react';
 import FuncMemoCompo from './memoComp';
 const FuncParentCompo = ()=>{
+
+  const [text,setText] = useState([{name:'james'}]);
   return(<>
      <h1>Parent Func Component</h1>
-     <FuncMemoCompo />
+     <FuncMemoCompo text={text} />
   </>);
 }
 export default FuncParentCompo
