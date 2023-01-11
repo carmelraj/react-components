a6ed8dcdd26876aa4dd9c476f53060628d01195b
carmelraj <carmelraj601@gmail.com>
2023-01-12 01:04:43 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-const rootElements = document.getElementById('root1');
-const root1 = createRoot(rootElements);
+const rootElements = document.getElementById('popupModal');
+const popupModal = createRoot(rootElements);
 root.render(
   <StrictMode>
     <App />
