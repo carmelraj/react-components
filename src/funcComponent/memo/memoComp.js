bba709c81b2da2d9c8d00673c45444d20b19db26
carmelraj <carmelraj601@gmail.com>
2023-01-11 17:57:22 +0530

    1 file modified, 1 added

---
+++ src/funcComponent/memo/memoComp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const FuncMemoCompo = ()=>{
+  return(<>
+     <p>Memo Component</p>
+  </>);
+}
+export default FuncMemoCompo
--- src/funcComponent/memo/parent.js
+++ src/funcComponent/memo/parent.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import FuncMemoCompo from './memoComp';
 const FuncParentCompo = ()=>{
   return(<>
-    <h1>Parent Func Component</h1>
+     <h1>Parent Func Component</h1>
+     <FuncMemoCompo />
   </>);
 }
 export default FuncParentCompo
