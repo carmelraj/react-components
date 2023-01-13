6c15f31ed860fc27aeb266475c0ebceebc3c28f0
carmelraj <carmelraj601@gmail.com>
2023-01-13 17:20:34 +0530

    1 file modified

--- src/RenderProps/user.js
+++ src/RenderProps/user.js
@@ -1,12 +1,5 @@
 import React,{Component} from 'react';
 class RenderPropsUser extends Component{
-  constructor(){
-    super(),
-    this.clickHandler = this.clickHandler
-  }
-  clickHandler(){
-    alert('Button Clicked');
-  }
   render(){
     return(<><h1>Render Props User</h1>
     {this.props.name(false)}
