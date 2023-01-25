978940bf9a25471097adcce889bb70322fd12f31
carmelraj <carmelraj601@gmail.com>
2023-01-25 17:17:01 +0530

    1 file modified

--- src/funcComponent/hooksCounter.js
+++ src/funcComponent/hooksCounter.js
@@ -7,11 +7,14 @@ const HooksCounter = () =>{
  
   return(<><h2>Hooks Counter</h2>
   <label>First Name</label>&nbsp;&nbsp;
- <input type="text" /> <br /><br />
+ <input type="text" value={name.firstName} 
+ onChange={e => setName({ ...name, firstName: e.target.value})} /> <br /><br />
  <label>Last Name</label>&nbsp;&nbsp;
- <input type="text" /><br /><br />
+ <input type="text" value={name.lastName}
+ onChange={e => setName({ ...name, lastName: e.target.value})} /><br /><br />
   <button onClick={}>Click me</button> 
   <p>Your name is - {name.firstName} {name.lastName}</p>
+  <p>{JSON.stringify(name)}</p>
   </>);
 }
 export default HooksCounter
