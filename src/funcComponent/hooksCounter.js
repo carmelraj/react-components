16898c2d3fb952ec3aa8eee4be7425752396aff9
carmelraj <carmelraj601@gmail.com>
2023-01-25 17:02:32 +0530

    1 file modified

--- src/funcComponent/hooksCounter.js
+++ src/funcComponent/hooksCounter.js
@@ -6,8 +6,12 @@ const HooksCounter = () =>{
 
  
   return(<><h2>Hooks Counter</h2>
- 
-  <button onClick={}>Click me</button>
+  <label>First Name</label>&nbsp;&nbsp;
+ <input type="text" /> <br /><br />
+ <label>Last Name</label>&nbsp;&nbsp;
+ <input type="text" /><br /><br />
+  <button onClick={}>Click me</button> 
+  <p>Your name is - {name.firstName} {name.lastName}</p>
   </>);
 }
 export default HooksCounter
